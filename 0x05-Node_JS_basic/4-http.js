9989bb3cfcb0350e3c52854d46f1627975627fcd
Ermi24et <lilermi24@gmail.com>
2024-04-30 20:35:02 +0300

    create a small HTTP server using Node's HTTP moduel

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello Holberton School!');
+    res.end();
+});
+app.listen(port);
+module.export = app;
