c0594a245fc9ff59f08c495b9bccc6854c10e47f
Ermi24et <lilermi24@gmail.com>
2024-04-30 20:43:46 +0300

    create a small HTTP server using Node's HTTP moduel

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,10 +3,10 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello Holberton School!');
-    res.end();
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 app.listen(port);
