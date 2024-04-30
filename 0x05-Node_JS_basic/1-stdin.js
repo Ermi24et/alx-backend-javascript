c38e617c42ca546e23a9eea8e82b11c544bbc912
Ermi24et <lilermi24@gmail.com>
2024-04-30 16:55:42 +0300

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding('utf-8');
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
 process.stdin.on('readable', () => {
   const inp = process.stdin.read();
   if (inp) {
     process.stdout.write(`Your name is: ${inp}`);
   }
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
