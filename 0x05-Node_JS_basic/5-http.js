27c5d39798fdf99de54947a6e14794ff63485f6d
Ermi24et <lilermi24@gmail.com>
2024-05-01 04:07:31 +0300

    create  a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,30 +1,50 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
-const hostname = '127.0.0.1';
-const port = 1245;
+async function countStudents(path) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(path, 'utf8')
+			.then((db) => {
+				let data;
+				const students = db.split('\n').filter((line) => line.trim() !== '');
+				data = `Number of students: ${students.length - 1}`;
+				const fields = {};
+				students.slice(1).forEach((student) => {
+					const field = student.split(',')[3];
+					const name = student.split(',')[0];
+					if (fields[field]) {
+						fields[field].push(name);
+					} else {
+						fields[field][name];
+					}
+				});
+				Object.keys(fields).forEach((key) => {
+					data = data.concat(`\nNumber of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
+				});
+				resolve(data);
+			})
+			.catch(() => {
+				reject(new Error('Cannot load the database'));
+			});
+	});
+}
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    let temp = 'This is the list of our students\n';
-    await countStudents(process.argv[2])
-      .then((mesg) => {
-        temp += mesg;
-        res.end(temp);
-      })
-      .catch((err) => {
-        temp += err.message;
-        res.end(temp);
-      });
-  }
+const app = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	if (req.url === '/') {
+		res.end('Hello Holberton School!');
+	} else if (req.url === '/students') {
+		countStudents(process.argv[2])
+			.then((data) => {
+				res.end(`This is the list of our students\n${data}`);
+			})
+			.catch((err) => {
+				res.end(`This is the list of our students\n${err.message}`);
+			});
+	}
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+app.listen(1245, () => {
+	// hello
 });
-
 module.exports = app;
