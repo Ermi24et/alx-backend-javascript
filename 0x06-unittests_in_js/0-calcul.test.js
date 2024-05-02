e199207874c4921c8594994d6ddcb0831baa9eb9
Ermi24et <lilermi24@gmail.com>
2024-05-02 23:16:24 +0300

    test file basic test with mocha

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,10 +3,10 @@ const calculateNumber = require('./0-calcul.js');
 const { describe, it } = require('node:test');
 
 describe('calculateNumber', () => {
-    it('it round the first argument', () => {
-        assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1, 3.7), 5);
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-        assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
+  it('it round the first argument', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
 });
