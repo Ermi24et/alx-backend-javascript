8e9ec03d484032372ddc96d7b8f539a091563d3a
Ermi24et <lilermi24@gmail.com>
2024-05-03 07:29:22 +0300

    basic test with mocha and node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,2 +1,5 @@
-function calculateNumber(a, b) { return Math.round(a) + Math.round(b) }
+const calculateNumber = (a, b) => {
+  return Math.round(a) + Math.round(b)
+};
+
 module.exports = calculateNumber;
