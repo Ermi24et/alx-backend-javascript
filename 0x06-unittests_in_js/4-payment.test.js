a75851588b0e29efb26ffde6d0e9353fe30988e4
Ermi24et <lilermi24@gmail.com>
2024-05-03 11:51:26 +0300

    stubs test

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('stubs', () => {
+    it('equal', () => {
+        const stubU = sinon.stub(Utils, 'calculateNumber');
+        stubU.returns(10);
+        const spyC = sinon.spy(console, 'log');
+
+        sendPaymentRequestToApi(100, 20);
+
+        expect(stubU.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        expect(spyC.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+        stubU.restore();
+        spyC.restore();
+    });
+});
