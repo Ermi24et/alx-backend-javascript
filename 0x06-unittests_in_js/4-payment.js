a0ebae79a242c177d8554554a0f77096cbbe65c2
Ermi24et <lilermi24@gmail.com>
2024-05-03 11:53:18 +0300

    stubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -4,5 +4,4 @@ const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${result}`);
 };
-
 module.exports = sendPaymentRequestToApi;
