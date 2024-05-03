6dc204cdb7dafb96b4b9c60486a82e846a431c0b
Ermi24et <lilermi24@gmail.com>
2024-05-03 11:39:19 +0300

    payment test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,5 +1,5 @@
 const sinon = require('sinon');
-import { expect } from 'chai';
+const { expect } = require('chai');
 
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
